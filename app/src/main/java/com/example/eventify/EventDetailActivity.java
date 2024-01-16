d58cb5fb5fd2c8825463d57a61212503ca5d14d9
Xue Liang <xueliangchong1@gmail.com>
2024-01-16 22:26:10 +0800

    CreateEvent

--- app/src/main/java/com/example/eventify/EventDetailActivity.java
+++ app/src/main/java/com/example/eventify/EventDetailActivity.java
@@ -1,6 +1,7 @@
 package com.example.eventify;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,10 +13,12 @@ public class EventDetailActivity extends AppCompatActivity {
 
     private TextView tvEventName;
     private TextView tvEventDescription;
-    private TextView tvEventTheme;
-    private TextView tvEventLocation;
+    private TextView tvEventTheme;;
     private TextView tvDateTime;
     private ImageView ivEventImage;
+    private Button btnInvite;
+    private Button btnEdit;
+    private Button btnDelete;
 
     private DatabaseReference databaseReference;
 
@@ -23,5 +26,13 @@ public class EventDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invitation_detail);
+
+        tvEventName=findViewById(R.id.tvEventName);
+        tvEventTheme=findViewById(R.id.tvEventTheme);
+        tvDateTime=findViewById(R.id.tvDateTime);
+        ivEventImage=findViewById(R.id.ivtEventImage);
+        btnInvite=findViewById(R.id.btnInvite);
+        btnEdit=findViewById(R.id.btnEdit);
+        btnDelete=findViewById(R.id.btnDelete);
     }
 }
