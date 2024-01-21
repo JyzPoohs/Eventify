ad884641334c70c1b7a5945cb3e3c09f416017bf
Xue Liang <xueliangchong1@gmail.com>
2024-01-21 17:11:30 +0800

    Delete bug Fixed

--- app/src/main/java/com/example/eventify/EventDetailActivity.java
+++ app/src/main/java/com/example/eventify/EventDetailActivity.java
@@ -15,12 +15,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import org.w3c.dom.Text;
 
@@ -83,7 +87,7 @@ public class EventDetailActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                // Handle errors
+
             }
         });
 
@@ -173,8 +177,29 @@ public class EventDetailActivity extends AppCompatActivity {
         builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                // Remove the event data from Realtime Database
+                myRef.child(id).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            Event event = dataSnapshot.getValue(Event.class);
+
+                            // Delete the image from Firebase Storage
+                            if (event != null && event.getImageUrl() != null && !event.getImageUrl().isEmpty()) {
+                                StorageReference storageRef = FirebaseStorage.getInstance().getReferenceFromUrl(event.getImageUrl());
+                                storageRef.delete();
+                            }
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+                // Remove the event from Realtime Database
                 myRef.child(id).removeValue();
-                Intent intent = new Intent(EventDetailActivity.this,EventListActivity.class);
+
+                Intent intent = new Intent(EventDetailActivity.this, EventListActivity.class);
                 startActivity(intent);
             }
         });
@@ -186,6 +211,7 @@ public class EventDetailActivity extends AppCompatActivity {
         });
         builder.create().show();
     }
+
     private void onNavigationIconClick() {
         Intent intent = new Intent(EventDetailActivity.this, SideMenuActivity.class);
         startActivity(intent);
